d1be1160f51e07785b0fb9a536621a6f7f5e08a0
Findana <findana.nafuka@gmail.com>
2022-06-27 22:40:44 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+//http://localhost:5000/
+app.use("/", user);
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connected and server running on port: ", PORT);
+    });
+  })
+  .catch((error) => console.log(error));
